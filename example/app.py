9f5842005158ab4999c9a82605d209f101d91657
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-09-22 23:01:51 +0900

    Add Usage

--- example/app.py
+++ example/app.py
@@ -8,9 +8,9 @@ def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
     return [b"<html><body><a href='/test'>Hello WSGI world!</a></body></html>"]
 
-app = Middleware(application, {
+wovn_app = Middleware(application, {
     'user_token': 'IRb6-',
     'secret_key': 'secret',
 })
 
-run_simple('0.0.0.0', 8000, app)
+run_simple('0.0.0.0', 8000, wovn_app)
